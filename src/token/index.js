d084d38e8249f2d4948ddab69dc8fa281e3fd1ba
Seongseokwon <pam9411@naver.com>
2021-03-26 14:29:19 +0900

    ✨ feat/29/access_token

---
+++ src/token/index.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+const tokenFunction = {
+  createAccessToken: (userInfo) => {
+    const accessToken = jwt.sign(userInfo, ACCESS_SECRET, { expiresIn: '2 h' });
+    return accessToken;
+  },
+};
+
+module.exports = tokenFunction;
