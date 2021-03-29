4d72eb190d6317e74a84baa0806f6fa2e30717a3
SungSeokMin <jkl154527@gmail.com>
2021-03-29 11:02:22 +0900

    ✨ feat/10/modify_userInfo

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -53,13 +53,15 @@ const userModels = {
         const { email, user_name, phone_number, profile_image, birthday } = JSON.parse(
           JSON.stringify(loginReq[0])
         )[0];
+
         const accessToken = tokenF.createAccessToken({
           email,
-          user_name,
-          phone_number,
-          profile_image,
+          userName: user_name,
+          phoneNumber: phone_number,
+          profileImage: profile_image,
           birthday,
         });
+        console.log(accessToken);
         return accessToken;
       }
       return false;
@@ -130,6 +132,40 @@ const userModels = {
       const result = await conn.query(getCardSql + getStackSql, [email, email]);
       return result[0];
     } catch (err) {
+      return err;
+    }
+  },
+  userInfoModify: async (userInfo) => {
+    const { email, password, profileIamge, phoneNumber } = userInfo;
+
+    try {
+      const conn = await connect();
+
+      if (password === undefined) {
+        const modifyUserInfoSql = `
+        UPDATE users SET profile_image=?, phone_number=? WHERE email = ?
+      `;
+
+        const nonePassword = await conn.query(modifyUserInfoSql, [
+          profileIamge,
+          phoneNumber,
+          email,
+        ]);
+        return { nonePassword };
+      }
+      const modifyUserInfoSql = `
+        UPDATE users SET password=?, profile_image=?, phone_number=? WHERE email = ?
+      `;
+
+      const existPassword = await conn.query(modifyUserInfoSql, [
+        password,
+        profileIamge,
+        phoneNumber,
+        email,
+      ]);
+      return { existPassword };
+    } catch (err) {
+      console.log(err);
       return err;
     }
   },
